1bf887636d2d02157020f4c1b25744f4f55f6178
Nightingale <solwork550@gmail.com>
2022-12-05 17:48:58 +0300

    test

--- terminal.py
+++ terminal.py
@@ -1,37 +1,64 @@
 from order import Order
-from pizza import * 
+from pizza import *
 from seller import Seller
-
+from drinks import *
+import time
 
 seller = Seller()
 
+sea = Seafood()
+bbq = BBQ()
+pep = Pepperoni()
+cola = Cola()
+tea_1 = TeaBlack200()
+tea_2 = TeaBlack300()
+tea_3 = TeaGreen200()
+tea_4 = TeaGreen300()
+
+menu = [sea, bbq, pep,cola, tea_1,tea_2,tea_3,tea_4]
+
 class Terminal:
-    def __init__(self):
-        self.menu = []
+    def __init__(self,menu):
+        self.menu = menu
         self.order = []
         self.ini = False
 
     def print_menu(self):
-        for i in range(len(self.menu)):
-            print(f'{i+1}. {self.menu[i]}')
-    
+            print('\n'*1)
+            for i in range(len(self.menu)):
+                
+                print(f'{i+1}. {self.menu[i]}')
+            print('\n'*1)
+
     def check_menu(self,order):
         while True:
             self.print_menu()
             us_inp = input('''
-            [Комманды y - подтвердить заказ, n - отменить заказ]
-            Введите код пиццы >>>>''').lower()
-            if us_inp == 'y':
-                if self.give_money() == True:
-                    order.perform()
-                break
+            [Комманды да - подтвердить заказ, нет - отменить заказ,состав - состав заказа ]
+            [стоп - остановить]
+            Введите код пиццы 
+            >>>>''').lower()
+            if us_inp == 'да':
+                if self.give_money(order) == True:
+                    self.order.append(order)
+                    order.perform(len(self.order))
+                    return
+                else:
+                    print('Продожить составлять заказ', "\n")
 
-            if us_inp == 'n':    
-                pass
-            try:
-                order.add_pizza(terminal.menu[int(us_inp)-1])
-            except:
-                print('Введен неверный код')
+            elif us_inp == 'нет':
+                return
+            elif  us_inp == 'состав':
+                order.suma()
+            elif  us_inp == 'стоп':
+                seller.time_close()
+                self.close(seller)
+                return    
+            else:
+                try:
+                    order.add_meal(terminal.menu[int(us_inp)-1])
+                except:
+                    print('Введен неверный код')
 
 
 
@@ -46,16 +73,16 @@ class Terminal:
             print('Такой код отсутствует')
         
 
-    def give_money(self):
-        ter_mes = input('Оплатить заказ [да/нет]')
+    def give_money(self,order):
+        order.suma()
+        ter_mes = input('Оплатить заказ [да/нет]').lower()
         if ter_mes == 'да':
             print('Оплата')
             return True
         elif ter_mes=='нет':
-            raise Exception('Выход из оплаты')
+            print('Выход из оплаты')
+            return False
      
-    def print_client_check(self, order):
-        order.suma()
 
     def print_check(self, seller):
         print(seller)
@@ -64,7 +91,7 @@ class Terminal:
         proceeds = 0
         print(f'Кол-во заказов {len(self.order)}')
         for i in range(len(self.order)):
-            proceeds += self.order[i].price
+            proceeds += self.order[i].sum_of_check
         print(f'Сумма за день {proceeds}')
 
             
@@ -75,10 +102,7 @@ class Terminal:
             Время открытия {seller.open_time}''')
             while self.ini:
                 order = Order()
-                self.check_menu()
-            
-        
-
+                self.check_menu(order)
         else:
             print('Смена закрыта , для начала зарегистрируйтесь в системе и откройте кассу')
         
@@ -94,8 +118,8 @@ class Terminal:
         else:
             print('Кассир не закрыл кассу')
 
-terminal = Terminal()
+
+
+terminal = Terminal(menu)
 seller.time_open()
 terminal.open(seller)
-seller.time_close()
-terminal.close(seller)
